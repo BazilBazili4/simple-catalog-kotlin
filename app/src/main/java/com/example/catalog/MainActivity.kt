34af875705ed9d87f86c10820fb79378c1f62c9d
kurlov-v <bazilevsvvv@gmail.com>
2020-09-28 23:34:07 +0300

    fixed bottom menu style

--- app/src/main/java/com/example/catalog/MainActivity.kt
+++ app/src/main/java/com/example/catalog/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.catalog
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,10 +21,17 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
                 R.id.navigation_home, R.id.navigation_favorite, R.id.navigation_list,
-                R.id.navigation_search, R.id.navigation_settings))
+                R.id.navigation_search, R.id.navigation_settings
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setItemIconTintList(null);
+
+
+
     }
 }
