16316f528907c568cc019fde8c8d92f1783c494e
kurlov-v <bazilevsvvv@gmail.com>
2020-09-29 21:02:14 +0300

    added navigation by buttons on home screen

--- app/src/main/java/com/example/catalog/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/catalog/ui/home/HomeFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
 import com.example.catalog.R
 
 class HomeFragment : Fragment() {
@@ -26,6 +28,12 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        val buttonToSearch: Button = root.findViewById(R.id.to_search_button)
+        buttonToSearch.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.navigation_search, null))
+
+        val buttonToList: Button = root.findViewById(R.id.to_list_button)
+        buttonToList.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.navigation_list, null))
+
         return root
     }
 }
