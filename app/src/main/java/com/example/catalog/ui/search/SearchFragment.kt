f117ae8ea036b9090db198f86a6244b8dd3c1287
kurlov-v <bazilevsvvv@gmail.com>
2020-09-30 16:04:28 +0300

    added inputs to search screen

--- app/src/main/java/com/example/catalog/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/catalog/ui/search/SearchFragment.kt
@@ -3,13 +3,14 @@ package com.example.catalog.ui.search
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnFocusChangeListener
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.catalog.R
-import com.google.android.material.textfield.TextInputEditText
 
 
 class SearchFragment : Fragment() {
@@ -24,7 +25,33 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val root = inflater.inflate(R.layout.fragment_search, container, false)
+        val spinnerCity: Spinner = root.findViewById(R.id.spinner_city)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.brew_array,
+            R.layout.spinner
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerCity.adapter = adapter
+        }
+
+        val spinnerDirect: Spinner = root.findViewById(R.id.spinner_direct)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.brew_array2,
+            R.layout.spinner
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinnerDirect.adapter = adapter
+        }
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
