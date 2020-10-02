d8b996f4b798d8b96b0c8effd1f9cf6580bccef6
kurlov-v <bazilevsvvv@gmail.com>
2020-10-02 18:00:54 +0300

    removed old code

--- app/src/main/java/com/example/catalog/ui/catalog/CatalogFragment.kt
+++ app/src/main/java/com/example/catalog/ui/catalog/CatalogFragment.kt
@@ -35,19 +35,6 @@ class CatalogFragment : Fragment() {
         viewModel =
             ViewModelProviders.of(this).get(CatalogViewModel::class.java)
 
-//        val textView: TextView = root.findViewById(R.id.greeting_text)
-//        viewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
-//
-//        val secondTextView: TextView = root.findViewById(R.id.second_text)
-//        viewModel.secondField.observe(viewLifecycleOwner, Observer {
-//            secondTextView.text = it
-//        })
-//
-//        val button: TextView = root.findViewById(R.id.button)
-//        button.setOnClickListener { viewModel.updateGreet(true, "fuck") }
-
         val cardHelper = CatalogCardHelper()
         val baseLayout: LinearLayout = root.findViewById(R.id.base_layout)
         baseLayout.addView(cardHelper.createCatalogCard(requireContext()))
